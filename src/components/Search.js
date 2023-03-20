be7f77d1d415f93ee908724b269ffa1a899b30e9
Rakha112 <rakhawibowo1998@gmail.com>
2023-03-20 21:08:41 +0700

    Create Search Component

---
+++ src/components/Search.js
@@ -0,0 +1,52 @@
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  useWindowDimensions,
+  Image,
+} from 'react-native';
+import React, {useState} from 'react';
+
+const Search = ({searchHandle}) => {
+  const {width} = useWindowDimensions();
+  const [city, setCity] = useState('');
+  return (
+    <View style={[styles.container, {width: width * 0.9}]}>
+      <Image
+        source={require('../assets/icons/SearchIcon.png')}
+        style={styles.searchIcon}
+      />
+      <TextInput
+        placeholder="Search..."
+        placeholderTextColor={'#505A7D'}
+        style={styles.input}
+        onChangeText={e => setCity(e)}
+        onSubmitEditing={() => {
+          searchHandle(city);
+        }}
+      />
+    </View>
+  );
+};
+
+export default Search;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    justifyContent: 'center',
+    maxWidth: 500,
+  },
+  input: {
+    padding: 12,
+    marginLeft: 30,
+    color: '#505A7D',
+  },
+  searchIcon: {
+    position: 'absolute',
+    width: 20,
+    height: 20,
+    marginLeft: 10,
+  },
+});
