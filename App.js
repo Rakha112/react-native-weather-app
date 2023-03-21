c395f4942e6a1141a9cb65600603089d56f99463
Rakha112 <rakhawibowo1998@gmail.com>
2023-03-21 20:23:50 +0700

    Add GestureHandlerRootView

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {useRef} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -5,6 +6,7 @@ import Onboarding from './src/screen/Onboarding';
 import Home from './src/screen/Home';
 import NextDay from './src/screen/NextDay';
 import Toast from './src/components/Toast';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 const App = () => {
   const toastRef = useRef(null);
   const Stack = createStackNavigator();
@@ -30,42 +32,44 @@ const App = () => {
   });
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Onboarding">
-        <Stack.Screen
-          name="Onboarding"
-          component={Onboarding}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="Home"
-          options={{
-            headerShown: false,
-            cardStyleInterpolator: customTransition,
-            transitionSpec: {
-              close: {animation: 'timing', config: {duration: 500}},
-              open: {animation: 'timing', config: {duration: 500}},
-            },
-          }}>
-          {props => <Home {...props} toast={toastRef} />}
-        </Stack.Screen>
-        <Stack.Screen
-          name="NextDay"
-          component={NextDay}
-          options={{
-            headerShown: false,
-            cardStyleInterpolator: customTransition,
-            transitionSpec: {
-              close: {animation: 'timing', config: {duration: 500}},
-              open: {animation: 'timing', config: {duration: 500}},
-            },
-          }}
-        />
-      </Stack.Navigator>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Onboarding">
+          <Stack.Screen
+            name="Onboarding"
+            component={Onboarding}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Home"
+            options={{
+              headerShown: false,
+              cardStyleInterpolator: customTransition,
+              transitionSpec: {
+                close: {animation: 'timing', config: {duration: 500}},
+                open: {animation: 'timing', config: {duration: 500}},
+              },
+            }}>
+            {props => <Home {...props} toast={toastRef} />}
+          </Stack.Screen>
+          <Stack.Screen
+            name="NextDay"
+            component={NextDay}
+            options={{
+              headerShown: false,
+              cardStyleInterpolator: customTransition,
+              transitionSpec: {
+                close: {animation: 'timing', config: {duration: 500}},
+                open: {animation: 'timing', config: {duration: 500}},
+              },
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
       <Toast ref={toastRef} />
-    </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
