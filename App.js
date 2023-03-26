e79fc09abaf4131ebdcc0ba3541639bbe4de4dd4
Rakha112 <rakhawibowo1998@gmail.com>
2023-03-26 14:53:49 +0700

    Change Navigation Transition

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import {StatusBar} from 'react-native';
 import React, {useRef} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 import Onboarding from './src/screen/Onboarding';
 import Home from './src/screen/Home';
 import NextDay from './src/screen/NextDay';
@@ -11,26 +11,6 @@ import {GestureHandlerRootView} from 'react-native-gesture-handler';
 const App = () => {
   const toastRef = useRef(null);
   const Stack = createStackNavigator();
-  const customTransition = ({current, next, layouts}) => ({
-    cardStyle: {
-      transform: [
-        {
-          translateX: current.progress.interpolate({
-            inputRange: [0, 1],
-            outputRange: [layouts.screen.width, 0],
-          }),
-        },
-        {
-          translateX: next
-            ? next.progress.interpolate({
-                inputRange: [0, 1],
-                outputRange: [0, -layouts.screen.width],
-              })
-            : 0,
-        },
-      ],
-    },
-  });
 
   return (
     <GestureHandlerRootView style={{flex: 1}}>
@@ -52,11 +32,7 @@ const App = () => {
             name="Home"
             options={{
               headerShown: false,
-              cardStyleInterpolator: customTransition,
-              transitionSpec: {
-                close: {animation: 'timing', config: {duration: 500}},
-                open: {animation: 'timing', config: {duration: 500}},
-              },
+              ...TransitionPresets.SlideFromRightIOS,
             }}>
             {props => <Home {...props} toast={toastRef} />}
           </Stack.Screen>
@@ -69,11 +45,7 @@ const App = () => {
                 backgroundColor: '#C4E2FE',
               },
               headerTintColor: '#505A7D',
-              cardStyleInterpolator: customTransition,
-              transitionSpec: {
-                close: {animation: 'timing', config: {duration: 500}},
-                open: {animation: 'timing', config: {duration: 500}},
-              },
+              ...TransitionPresets.SlideFromRightIOS,
             }}
           />
         </Stack.Navigator>
